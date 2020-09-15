9e4d609323789ae894f74046f43a4617b40af139
jaideepghosh <er.jaideepghosh@gmail.com>
2020-09-15 11:22:58 +0530

    feat(app):Moved the scripts to src folder.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const config = require("./config.json");
+const config = require("../config.json");
 //set config in the app.
 for (let key in config) {
 	app.set(key, config[key]);
